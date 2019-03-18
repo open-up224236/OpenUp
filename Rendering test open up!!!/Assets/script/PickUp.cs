64db1e260238c5d3d0ac36dbd8ad5bf6b9558c5b
Na224248 <fafuang@gmail.com>
2019-03-18 10:59:55 +0700

    renovate inventory panel

--- Rendering test open up!!!/Assets/script/PickUp.cs
+++ Rendering test open up!!!/Assets/script/PickUp.cs
@@ -1,20 +1,38 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.SceneManagement;
 using UnityEngine;
+
 
 public class PickUp : MonoBehaviour
 {
     public GameObject ObjectPick;
+    public GameObject InvenObject;
+    public GameObject need;
+    public string scene;
+    int over;
+
+    static int InvenCount = 0;
 
     void OnMouseDown()
     {
         //if (require == "")
         //{
-        ObjectPick.SetActive(false);
-        Debug.Log("pick helmet");
-        //}
-        //check if have require item or not
-
+        if(/*need != null && */need.activeSelf == true)
+        {
+            ObjectPick.SetActive(false);
+            InvenObject.SetActive(true);
+            InvenCount++;
+            if (InvenCount >= 1)
+            {
+                InvenObject.transform.Translate((InvenCount - 1) * 1, 0, 0);
+            }
+            Debug.Log(InvenCount);
+        }
+        else
+        {
+            SceneManager.LoadScene(scene);
+        }
     }
 
 }
