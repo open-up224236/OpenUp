45f0cce97f795742ca5a37fa06cad46690d99969
Na224248 <fafuang@gmail.com>
2019-03-25 23:40:53 +0700

    Finish Sequence and cut scene
    - All level 1 Puzzle Sequence include.
    - All Cut scene include

--- Rendering test open up!!!/Assets/script/Innventory.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class Innventory : MonoBehaviour
-{
-    public bool[] isFull;
-    public GameObject[] slots;
-}
--- Rendering test open up!!!/Assets/script/SetInActive.cs
+++ Rendering test open up!!!/Assets/script/SetInActive.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class InventControl : MonoBehaviour
+public class SetInActive : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
